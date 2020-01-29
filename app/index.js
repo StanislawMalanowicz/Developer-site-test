ac846956ce4b10ecc82bdafe48fb026247ac1edc
Your Name <you@example.com>
2020-01-29 09:52:56 +0100

    Responsive website

--- app/index.js
+++ app/index.js
@@ -9,7 +9,19 @@ console.log('Boilerplate is working!');
 
 
 // const translateBtn = document.querySelector('.translate-button');
+// const pl = document.querySelectorAll('.pl');
+// pl.forEach(el => el.style.display === "none");
 
 // translateBtn.addEventListener("click", ()=> {
-
+//     console.log("translate: ", pl);
+//     const en = document.querySelectorAll('.en');
+//     // if(pl[0].style.display === "block"){
+//         pl.forEach(el => el.style.display === "none");
+//         en.forEach(el => el.style.display === "block");
+//         return
+//     // }
+//     //  en.forEach(el => el.style.display === "none");
+//     //  pl.forEach(el => el.style.display === "block");
+//     //  return;
+    
 // })
